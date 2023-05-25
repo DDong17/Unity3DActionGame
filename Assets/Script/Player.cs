1f14d8736247a00ea906c6cd422d3771130a9fd3
DongHo Hwang <ghkdehdgh11@gmail.com>
2023-05-25 14:00:48 +0900

    Player 방향키 초기세팅

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed; //public 사용하면 유니티에 업로드 됌
+    float hAxis;
+    float vAxis;
+
+    Vector3 moveVec;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        hAxis = Input.GetAxisRaw("Horizontal");
+        vAxis = Input.GetAxisRaw("Vertical");
+
+        moveVec = new Vector3(hAxis,0,vAxis).normalized; //대각선 움직임까지 진행할때 사용하는 normalized
+
+        transform.position += moveVec*speed*Time.deltaTime;
     }
 }
