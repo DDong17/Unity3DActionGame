6461aab5f684780df3540623998d180210646b3d
DongHo Hwang <ghkdehdgh11@gmail.com>
2023-07-10 17:09:31 +0900

    Update Player.cs
    Jump Apply

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -8,31 +8,55 @@ public class Player : MonoBehaviour
     float hAxis;
     float vAxis;
     bool wDown;
+    bool jDown;
 
     Vector3 moveVec;
-
+    Rigidbody rigid;// 물리 효과를 위해 Rigidbody 변수 선언 후 초기화
     Animator anim;
 
     void Awake()
     {
+        rigid = GetComponent<Rigidbody>();
         anim = GetComponentInChildren<Animator>();
     }
 
-    
+
     void Update()
+    {
+        GetInput();
+        Move();
+        Turn();
+        Jump();
+
+    }
+    void GetInput()
     {
         hAxis = Input.GetAxisRaw("Horizontal");
         vAxis = Input.GetAxisRaw("Vertical");
         wDown = Input.GetButton("Walk");
+        jDown = Input.GetButtonDown("Jump");
+    }
 
-        moveVec = new Vector3(hAxis,0,vAxis).normalized; //대각선 움직임까지 진행할때 사용하는 normalized
+    void Move()
+    {
+        moveVec = new Vector3(hAxis, 0, vAxis).normalized; //대각선 움직임까지 진행할때 사용하는 normalized
 
-        transform.position += moveVec*speed* (wDown ? 0.3f : 1f) *Time.deltaTime;
+        transform.position += moveVec * speed * (wDown ? 0.3f : 1f) * Time.deltaTime;
 
-        anim.SetBool("isRun",moveVec != Vector3.zero);
-        anim.SetBool("isWalk",wDown);
+        anim.SetBool("isRun", moveVec != Vector3.zero);
+        anim.SetBool("isWalk", wDown);
+    }
 
+    void Turn()
+    {
         //LookAt: 지정된 벡터를 향해서 회전시켜주는 함수
-        transform.LookAt(transform.position+moveVec);
+        transform.LookAt(transform.position + moveVec);
+    }
+
+    void Jump()
+    {
+        if (jDown){
+            rigid.AddForce(Vector3.up * 15,ForceMode.Impulse);
+        }
     }
 }
