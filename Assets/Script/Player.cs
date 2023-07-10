87ba54f8b0f46c5181efa4a2e40668069d41bd1e
DongHo Hwang <ghkdehdgh11@gmail.com>
2023-07-10 18:09:36 +0900

    Dodge
    Dodge Apply

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -11,8 +11,10 @@ public class Player : MonoBehaviour
     bool jDown;
 
     bool isJump;
+    bool isDodge;
 
     Vector3 moveVec;
+    Vector3 dodgeVec;
     Rigidbody rigid;// 물리 효과를 위해 Rigidbody 변수 선언 후 초기화
     Animator anim;
 
@@ -29,6 +31,7 @@ public class Player : MonoBehaviour
         Move();
         Turn();
         Jump();
+        Dodge();
 
     }
     void GetInput()
@@ -43,6 +46,8 @@ public class Player : MonoBehaviour
     {
         moveVec = new Vector3(hAxis, 0, vAxis).normalized; //대각선 움직임까지 진행할때 사용하는 normalized
 
+        if(isDodge)
+            moveVec=dodgeVec;
         transform.position += moveVec * speed * (wDown ? 0.3f : 1f) * Time.deltaTime;
 
         anim.SetBool("isRun", moveVec != Vector3.zero);
@@ -57,20 +62,44 @@ public class Player : MonoBehaviour
 
     void Jump()
     {
-        if (jDown && !isJump){ // !isJump 가 False 일때
-            rigid.AddForce(Vector3.up * 15,ForceMode.Impulse);
+        if (jDown && moveVec==Vector3.zero && !isJump && !isDodge)
+        { // !isJump 가 False 일때
+            rigid.AddForce(Vector3.up * 15, ForceMode.Impulse);
             anim.SetBool("isJump", true);
             anim.SetTrigger("doJump");
-            isJump =true;
+            isJump = true;
 
         }
     }
-    void OnCollisionEnter(Collision collision) {
 
-        if(collision.gameObject.tag =="Floor"){
-            anim.SetBool("isJump", false);
-            isJump =false;
+    void Dodge()
+    {
+        if (jDown && moveVec!=Vector3.zero && !isJump && !isDodge )
+        { 
+            dodgeVec=moveVec;
+            speed *= 2;
+
+            anim.SetTrigger("doDodge");
+            isDodge = true;
+
+            Invoke("DodgeOut", 0.4f);//시간차 함수 호출
+
         }
-        
+    }
+
+    void DodgeOut()
+    {
+        speed *= 0.5f;
+        isDodge = false;
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.tag == "Floor")
+        {
+            anim.SetBool("isJump", false);
+            isJump = false;
+        }
+
     }
 }
